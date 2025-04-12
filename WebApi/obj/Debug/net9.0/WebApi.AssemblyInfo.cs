9c7312f2aab4e018357988ab47966b844b5ea462
Alesya Shashko <alesya@quantumreverse.com>
2025-04-12 13:19:15 +0300

    update

--- WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5777c0ad737deee38650f60ca0647eff8f432ce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7f2507296fa9c81bbae8db0955980e8c7122b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
